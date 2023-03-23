5f1f9d8ca35ef2785d3b3f9a42a40563c73b3844
JuBa012 <111688680+JuBa012a@users.noreply.github.com>
2023-03-23 08:43:50 +0100

    Add files via upload

--- Seite/script.java
+++ Seite/script.java
@@ -2,11 +2,12 @@ function toggleLightDark() {
   $('.content').toggleClass('dark-mode');
   $('.toggledarkbutton').toggleClass('dark-mode toggledarkbutton-dark');
   $('.header').toggleClass('header-dark');
+  $('.navbar').toggleClass('navbar-dark');
 
   if(localStorage.getItem('darkmode')==null){
   window.localStorage.setItem('darkmode','1');
   } else {
-	 window.localStorage.removeItem('darkmode'); 
+	 window.localStorage.removeItem('darkmode');
   }
   if(localStorage.getItem('darkmode')==null){
 	$('#toggledarkicon').attr('src','Photos/toggledarklight.png');
